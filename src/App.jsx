0ea30332a171f9b2d509f3da6858e8e2f843fd9d
rjwallace <WallaceRic@novelis.com>
2024-10-15 12:26:36 -0400

    Add grid to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { Flex } from "@aws-amplify/ui-react";
+import { Grid } from "@aws-amplify/ui-react";
 import WelcomeView from './views/WelcomeView';
 import FooterView from './views/FooterView';
 import "@aws-amplify/ui-react/styles.css";
 
 export default function App() {
   return (
-    <Flex
-        direction="column"
-        justifyContent="space-evenly"
+    <Grid
+        templateRows="85% 15%"
         alignItems="center"
-        alignContent="stretch"
         wrap="nowrap"
         gap="2rem"
         backgroundColor="white"
@@ -18,12 +16,14 @@ export default function App() {
         height="90vh"
       >
         {/* Header?*/}
+        
         {/* // Top Center - Welcome screen*/}
         <WelcomeView />
         {/* // Below show Company description/overview*/}
+
         {/* // Footer*/}
         <FooterView />
 
-      </Flex>
+      </Grid>
   );
 }
--- src/views/FooterView.jsx
+++ src/views/FooterView.jsx
@@ -2,11 +2,10 @@ import { Flex, Button, Text, ButtonGroup } from "@aws-amplify/ui-react";
 
 export default function FooterView() {
   return (
-    <Flex direction="column"
-      >
+    <Flex direction="column" alignItems="center" >
 
         {/* Buttons: Privacy Policy, About, Contact */}
-        <ButtonGroup size="small" direction="row" alignSelf="center" border="none">
+        <ButtonGroup size="small" direction="row" border="none">
             <Button border="none">Policies</Button>
             <Button border="none">About</Button>
             <Button border="none">Contact</Button>
