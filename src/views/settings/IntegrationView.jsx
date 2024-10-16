620f7a238c2b6ff4ad1005405660f1f0a4b768cd
rjwallace <WallaceRic@novelis.com>
2024-10-16 16:04:18 -0400

    Add Settings skeleton

---
+++ src/views/settings/IntegrationView.jsx
@@ -0,0 +1,19 @@
+import { Grid } from "@aws-amplify/ui-react";
+
+export default function IntegrationView() {
+  return (
+    <Grid
+      templateRows="50% 50%"
+      alignItems="center"
+      wrap="nowrap"
+      margin={"10px"}
+    >
+      {/* Icon with connected status */}
+      {/* Title */}
+      {/* Description */}
+      {/* Sync or Delete Button */}
+      
+
+    </Grid>
+  );
+}
---
+++ src/views/settings/SettingsPage.jsx
@@ -0,0 +1,20 @@
+import { Grid } from "@aws-amplify/ui-react";
+
+export default function SettingsPage() {
+  return (
+    <Grid
+      templateRows="50% 50%"
+      alignItems="center"
+      wrap="nowrap"
+      margin={"10px"}
+    >
+      {/* Integrations & Workflows */}
+      {/* Active Campaign */}
+      
+      {/* Gong */}
+      {/* ChatGPT */}
+      
+
+    </Grid>
+  );
+}
