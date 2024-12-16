19d6d793c6bc72485a132251c6eb4242288393d0
rjwallace <WallaceRic@novelis.com>
2024-12-16 14:49:18 -0500

    #17: wip: get contacts for signed in user

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -25,6 +25,7 @@ const Dashboard = () => {
     const fetchContacts = async () => {
       try {
         const res = await client.queries.LoadContacts();
+        if (res.errors) throw res.errors;
         setContacts(JSON.parse(res.data).contacts);
       } catch (err) {
         console.log(err);
