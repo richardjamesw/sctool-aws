c319bf5fcd25508fb4864a879bdfdda8a37c67ee
rjwallace <WallaceRic@novelis.com>
2024-12-17 15:30:27 -0500

    #17: get key and url data

--- amplify/functions/acLoadContacts/handler.ts
+++ amplify/functions/acLoadContacts/handler.ts
@@ -2,7 +2,7 @@ import type { Handler } from 'aws-lambda';
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../../amplify_outputs.json';
-import { type Schema } from "../../data/resource";
+import { type Schema } from '../../data/resource';
 import { generateClient } from 'aws-amplify/data';
 import { env } from '$amplify/env/post-confirmation';
 import axios from 'axios';
@@ -10,37 +10,28 @@ import axios from 'axios';
 Amplify.configure(outputs);
 
 export const handler: Handler = async (event, context) => {
-    let jwt = event.requestContext.authorizer?.jwt?.claims?.['id_token'];
-    if (!jwt) {
-      const authHeader = event.request.headers['Authorization'];
-      jwt = authHeader;
-    }
+  const jwt = event.request.headers['authorization'];
+  
+  const client = generateClient<Schema>({
+    authMode: 'identityPool',
+    authToken: jwt
+  });
+  const users = await client.models.UserProfile.list(); // todo confirm profileOwner matches? test if other users are coming in or just the one?
+  const prof = users.data[0];
+  // pull data from AC with user's URL and Key
+  const url = prof.acUrl;
+  const key = prof.acKey;
+  //const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
+  //const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
 
-    const client = generateClient<Schema>({
-      authMode: "identityPool",
-      authToken: jwt,
-    });
-
-    const users = await client.models.UserProfile.list({
-      authMode: "identityPool",
-      authToken: jwt,
-    }); // todo confirm profileOwner matches? test if other users are coming in or just the one?
-    const prof = users.data[0];
-
-    // pull data from AC with user's URL and Key
-    const url = prof.acUrl;
-    const key = prof.acKey;
-    //const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
-    //const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
-
-    // todo verify url format
-    let res = await axios.get(url + '/contacts', {
-        headers: {
-          'Api-Token': key,
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    });
-
-    return JSON.stringify(res.data);
-}
+  // todo verify url format
+  let res = await axios.get(url + '/contacts', {
+      headers: {
+        'Api-Token': key,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+  });
+  
+  return JSON.stringify(res.data);
+};
