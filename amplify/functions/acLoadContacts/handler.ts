1d750e85a17224f41628c5b7338a26a15a610d48
rjwallace <WallaceRic@novelis.com>
2024-12-20 11:08:31 -0500

    Build Fix 2: acLoadContacts\handler.ts

--- amplify/functions/acLoadContacts/handler.ts
+++ amplify/functions/acLoadContacts/handler.ts
@@ -1,18 +1,35 @@
-import type { Handler } from 'aws-lambda';
 import axios from 'axios';
+import { type Schema } from '../../data/resource';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/data';
+import outputs from '../../../amplify_outputs.json';
+import type { Handler } from 'aws-lambda';
+
+Amplify.configure(outputs);
 
 export const handler: Handler = async (event, context) => {
-    // pull data from AC with user's URL and Key
-    //const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
-    //const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
+  const jwt = event.request.headers['authorization'];
+  
+  const client = generateClient<Schema>({
+    authMode: 'identityPool',
+    authToken: jwt
+  });
+  const users = await client.models.UserProfile.list(); // todo confirm profileOwner matches? test if other users are coming in or just the one?
+  const prof = users.data[0];
+  // pull data from AC with user's URL and Key
+  const url = prof.acUrl;
+  const key = prof.acKey;
+  //const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
+  //const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
 
-    let res = await axios.get('https://sduplessis12312.api-us1.com/api/3/contacts', {
-        headers: {
-          'Api-Token': '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    });
-
-    return JSON.stringify(res.data);
-}
+  // todo verify url format
+  let res = await axios.get(url + '/contacts', {
+      headers: {
+        'Api-Token': key,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+  });
+  
+  return JSON.stringify(res.data);
+};
