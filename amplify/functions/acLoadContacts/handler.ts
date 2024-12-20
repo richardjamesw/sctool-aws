6a00d859508dd584129bd1710fc409d1f7426259
rjwallace <WallaceRic@novelis.com>
2024-12-20 14:20:25 -0500

    Actual build fix

--- amplify/functions/acLoadContacts/handler.ts
+++ amplify/functions/acLoadContacts/handler.ts
@@ -1,26 +1,29 @@
 import axios from 'axios';
-import { type Schema } from '../../data/resource';
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/data';
-//import outputs from '../../../amplify_outputs.json';
 import type { Handler } from 'aws-lambda';
+import { env } from '$amplify/env/acLoadContacts';
+import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+import { Amplify, ResourcesConfig } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/data';
+import { type Schema } from "../../data/resource";
 
-//Amplify.configure(outputs);
+const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
+Amplify.configure(resourceConfig as ResourcesConfig, libraryOptions);
 
 export const handler: Handler = async (event, context) => {
-  //const jwt = event.request.headers['authorization'];
+  const jwt = event.request.headers['authorization'];
   
-  //const client = generateClient<Schema>({
-  //  authMode: 'identityPool',
-  //  authToken: jwt
-  //});
-  //const users = await client.models.UserProfile.list(); // todo confirm profileOwner matches? test if other users are coming in or just the one?
-  //const prof = users.data[0];
+  const client = generateClient<Schema>({
+    authMode: 'identityPool',
+    authToken: jwt
+  });
+
+  const users = await client.models.UserProfile.list(); // todo confirm profileOwner matches? test if other users are coming in or just the one?
+  const prof = users.data[0];
   // pull data from AC with user's URL and Key
-  //const url = prof.acUrl;
-  //const key = prof.acKey;
-  const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
-  const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
+  const url = prof.acUrl;
+  const key = prof.acKey;
+  //const url = 'https://sduplessis12312.api-us1.com/api/3/contacts';
+  //const key = '2e816a5d87518f84abb715eed5698e126d0c6c5617a2d1bb277cc45e2e10cd983dac276b';
 
   // todo verify url format
   let res = await axios.get(url + '/contacts', {
