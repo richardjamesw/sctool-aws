fab4b839ede650e895bcf59d59a4861b525693be
rjwallace <WallaceRic@novelis.com>
2025-04-03 11:47:09 -0400

    #40: remove some unused projects

--- src/index.jsx
+++ src/index.jsx
@@ -14,16 +14,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/700.css';
 
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-
 // style + assets
 import 'assets/scss/style.scss';
 import reportWebVitals from 'reportWebVitals';
